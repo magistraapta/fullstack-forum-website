1e20f763ca815cf41595b593345325e2268305d4
Magistra <magistraagis@gmail.com>
2023-11-08 14:44:54 +0800

    init

---
+++ app/api/user/route.ts
@@ -0,0 +1,43 @@
+import { db } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+import {hash} from 'bcrypt'
+export async function POST(req: NextRequest, ) {
+    
+    try {
+        const body = await req.json()
+        const {username,email, password} = body;
+
+        const existingEmail = await db.user.findUnique({
+            where: {
+                email: email
+            }
+        })
+
+        if (existingEmail) {
+            return NextResponse.json({message: 'email already used'}, {status: 401})
+        }
+        const existingUsername = await db.user.findUnique({
+            where: {
+                username: username
+            }
+        })
+
+        if (existingUsername) {
+            return NextResponse.json({message: 'username already used'}, {status: 401})
+        }
+
+        const hashPassword = await hash(password, 10)
+        const result = await db.user.create({
+            data:{
+                username,
+                email,
+                password: hashPassword
+            }
+        })
+
+        return NextResponse.json({user: result, message: 'sucess create user'}, {status: 201});
+    } catch (error) {
+        
+    }
+    return NextResponse.json({message: 'success'})
+}
